05a55e6e4bbd739357b5593ebba2dd4b7c91d6df
Igor Kuzmenko <kuzzmi@gmail.com>
2015-02-07 02:28:21 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var wit = require('node-wit');
+var fs = require('fs');
+var ACCESS_TOKEN = "OLTQRQAU6E4K5N2JJWZZJ7HAOHJV72XA";
+
+console.log("Sending text & audio to Wit.AI");
+
+var stream = fs.createReadStream('hey_yeva.wav');
+wit.captureSpeechIntent(ACCESS_TOKEN, stream, "audio/wav", function(err, res) {
+    console.log("Response from Wit for audio stream: ");
+    if (err) console.log("Error: ", err);
+    console.log(JSON.stringify(res, null, " "));
+});
