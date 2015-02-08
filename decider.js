043e6615fbb3a1a36e8bdb6f43d992d89e6f6877
Igor Kuzmenko <kuzzmi@gmail.com>
2015-02-08 18:18:44 +0100

    Updates

--- decider.js
+++ decider.js
@@ -1,8 +1,11 @@
 var child = require('child_process');
+var request = require('request');
 
 var say = function(what) {
     child.spawn('./send_and_play.sh', [what + ', sir']);
 }
+
+var apiUrl = 'http://192.168.0.4:3000';
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
@@ -11,7 +14,7 @@ process.stdin.on('data', function(data) {
     var json = JSON.parse(data);
     var outcome = json.outcomes[0];
 
-    console.log(outcome);
+    console.log(data);
 
     if (outcome) {
         switch (outcome.intent) {
@@ -19,7 +22,33 @@ process.stdin.on('data', function(data) {
                 say(outcome.entities.greeting_phrase[0].value);
                 break;
             case 'weather_forecast':
-                say('I cannot predict weather yet');
+                request(apiUrl + '/weather', function(req, resp) {
+                    var data = resp.body.split(':');
+                    say('It is ' + data[0] + ' outside and ' + data[1]);
+                });
+
+                break;
+            case 'media':
+                var type = outcome.entities.media_type[0].value;
+                var action = outcome.entities.action[0].value;
+                var property = outcome.entities.media_propery ?
+                    outcome.entities.media_propery[0].value : undefined;
+
+                request(apiUrl + '/media/' + type + '/query/unwatched=1', function(req, resp) {
+                    var movies = JSON.parse(resp.body).video;
+
+                    switch (action) {
+                        case 'count':
+                            say('You have ' + movies.length + ' movies to watch');
+                            break;
+                        case 'read':
+                            var titles = movies.map(function(a) {
+                                return a.attributes.title;
+                            }).join('. ');
+                            say('You have these movies: ' + titles);
+                            break;
+                    }
+                });
                 break;
             case 'tell_time':
 
@@ -32,7 +61,7 @@ process.stdin.on('data', function(data) {
                         if (minutes < 30) {
                             return minutes + ' minutes past ' + hours;
                         } else {
-                            return minutes + ' to ' + (hours + 1);
+                            return (60 - minutes) + ' to ' + (hours + 1);
                         }
                     } else {
                         return hours + ' hours';
