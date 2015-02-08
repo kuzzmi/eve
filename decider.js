ab25c7fa2940f6996523b28a75e2ed5eb133470f
Igor Kuzmenko <kuzzmi@gmail.com>
2015-02-08 12:43:16 +0000

    Added decider

---
+++ decider.js
@@ -0,0 +1,5 @@
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(data) {
+    process.stdout.write(data);
+});
---
+++ send_and_play.sh
@@ -0,0 +1,1 @@
+node ivona.js "$1" && play -q temp.mp3 && rm temp.mp3
