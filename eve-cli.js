9c77b4c810997524e4914268fba741c892a60a16
Igor Kuzmenko <kuzzmi@gmail.com>
2015-02-10 17:43:12 +0100

    [Eve] Uploaded at Tue Feb 10 2015 17:43:12 GMT+0100 (W. Europe Standard Time)

--- eve-cli.js
+++ eve-cli.js
@@ -9,12 +9,58 @@ var rl = readline.createInterface({
     output: process.stdout
 });
 
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$P""*^^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$*``$P          ""**^^$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$*`    $                  `*^$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$P`      d`                      `^$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$P`      ,d`                          `T$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$P`     ,sP`                              T$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$P     -*`                                  T$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$P                                            `^$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$                                                T$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$                                                   $$$$$$$$$$$$');
+printFaceLines('$$$$$$;                                                   :$$$$$$$$$$$');
+printFaceLines('$$$$$;    _.------._                        _.------._     :$$$$$$$$$$');
+printFaceLines('$$$$$  .*`          `*-.                .-*`          `*.   $$$$$$$$$$');
+printFaceLines('$$$$$ /    lg+=ggg+.    `.    _.._    .`    .+ggg=+gl    \\  $$$$$$$$$$');
+printFaceLines('$$$$$:     $;  $$T T$*.   \\,+*""""*+,/   .*$$T T$  :$     ;$$$$$$$$$$$');
+printFaceLines('$$$$$:     `b  `T$sP`  \\   ;        :   /  `T$sP`  d`     ; $$$$$$$$$$');
+printFaceLines('$$$$$ \\      *+.       `   ;        :   `       .+*      /  $$$$$$$$$$');
+printFaceLines('$$$$$  `.                 /          \\                 .`  :$$$$$$$$$$');
+printFaceLines('$$$$$    `*-._       _.-*`            `*-._       _.-*`    $$$$$$$$$$$');
+printFaceLines('$$$$$         """""""                      """""""         $$$$$$$$$$$');
+printFaceLines('$$$$$                                                      $$$$$$$$$$$');
+printFaceLines('$$$$$$                                                    $$$$$$$$$$$$');
+printFaceLines('$$$$$$;                                                  :$$$$$$$$$$$$');
+printFaceLines('$$$$$$$                    ;        :        $$          $$$$$$$$$$$$$');
+printFaceLines('$$$$$$$                    `*-.__.-*`                    $$$$$$$$$$$$$');
+printFaceLines('$$EVE$$b                                                d$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$b                  _.._   _.._                 d$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$b               .d$$$$$g$$$$$b.              d$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$b.           .d$$$$$$$$$$$$$$$b.          .d$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$b.          `^$$$$$$$$$$$$$^`         .d$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$b.           "*^$$$$$^*"          .d$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$b.                            .d$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$b.                        .d$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$b.                    .d$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$b.                .d$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$+.          .+$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$gggggggg$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+printFaceLines('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+
 rl.prompt(true);
+
+function printFaceLines(line) {
+    console.log(color(line, "bold+magenta_bg"));
+};
 
 function output(msg) {
     process.stdout.clearLine();
     process.stdout.cursorTo(0);
-    console.log(color(msg, "cyan"));
+    console.log(color(msg, "magenta"));
     rl.prompt(true);
 }
 
